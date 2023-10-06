a5a02ae9a457677544f6e9a801faba3b11352743
Roman <toolatet@gmail.com>
2023-10-06 14:04:45 +0700

    init

---
+++ src/test/java/com/example/webjava/BankServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.webjava;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankServiceTest {
+
+    private BalanceRepository balanceRepository = new BalanceRepository();
+    private BankService bankService = new BankService(balanceRepository);
+
+
+    @Test
+    void getBalance() {
+        final BigDecimal balance = bankService.getBalance(1L);
+        assertEquals(balance, BigDecimal.TEN);
+    }
+    @Test
+    void addMoney() {
+        final BigDecimal balance = bankService.addMoney(1L, BigDecimal.ONE);
+        assertEquals(balance, BigDecimal.valueOf(11));
+    }
+}
